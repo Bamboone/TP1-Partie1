208f1d27c51dc13316a10417517c0e5be7f6d9f5
Gabriel Lévesque-Duval <glevesqueduval@gmail.com>
2019-01-28 08:49:13 -0500

    Ajouts des champs d'instances de la classe CV

--- src/CV.java
+++ src/CV.java
@@ -1,4 +1,14 @@
 
 public class CV {
 
+	private String nom;
+	private String prenom;
+	private String formation;
+	private int nbAnnees;
+	private String[] competences;
+	private String attentes;
+	
+	
+	
+	
 }
