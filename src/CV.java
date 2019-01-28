f4970a0de5c6f16d9cc7e7acbff1b3e7c15cc380
Gabriel Lévesque-Duval <glevesqueduval@gmail.com>
2019-01-28 08:54:58 -0500

    Ajout du constructeur CV

--- src/CV.java
+++ src/CV.java
@@ -8,6 +8,15 @@ public class CV {
 	private String[] competences;
 	private String attentes;
 	
+	public CV(String pNom, String pPrenom, String pFormation, int pNbAnnees, String[] pCompetences, String pAttentes) {
+		nom = pNom;
+		prenom = pPrenom;
+		formation = pFormation;
+		nbAnnees = pNbAnnees;
+		competences = pCompetences;
+		attentes = pAttentes;
+		
+	}
 	
 	
 	
