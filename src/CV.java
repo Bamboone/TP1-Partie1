0c8fc0085b8a77c0425b37647046d2c37904b447
Gabriel Lévesque-Duval <glevesqueduval@gmail.com>
2019-01-28 09:11:28 -0500

    Ajout de la méthode main()

--- src/CV.java
+++ src/CV.java
@@ -19,16 +19,25 @@ public class CV {
 	}
 	
 	public void affiche() {
-		System.out.println("Nom: " + nom +
+		System.out.print("Nom: " + nom +
 						   "\nPrénom: " + prenom +
 						   "\nFormation: " + formation +
 						   "\nExpériences de travail: " + nbAnnees + "ans");
-		System.out.println("\nCompetences: ");
+		System.out.print("\nCompetences: ");
 		for(int i=0; i<competences.length; i++) {
-			System.out.print(competences[i] + ", ");
+			System.out.print(competences[i] + " ");
 		}
-		System.out.println();
-		System.out.println("\nAttentes: " + attentes);
+		System.out.println("\nAttentes: " + attentes +"\n\n");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Bienvenue chez Barrette!\n");
+		String[] compGabriel = new String[] {"Programmation Objet", "C#", "Java", "HTML"};
+		String[] compIlyas = new String[] {"Programmation Objet", "C#", "Java", "HTML"};
+		CV gabriel = new CV("Lévesque-Duval", "Gabriel", "Techiques de l'informatique", 2, compGabriel, "Apprendre plus de choses sur la programmation.");
+		CV ilyas = new CV("Elamraoui", "Ilyas", "Techiques de l'informatique", 2, compIlyas, "Apprendre à gérer Git");
+		gabriel.affiche();
+		ilyas.affiche();
 	}
 	
 }
